2db063aef1277d606ea720cac91874c111fd9d19
Q♡Q♠ <76552431+4Bet-Allin@users.noreply.github.com>
2021-01-06 01:37:49 +0900

    https://leetcode.com/problems/palindrome-number/

---
+++ Palindrome Number.py
@@ -0,0 +1,8 @@
+﻿class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x == int(str(x)[::-1]):
+            return True
+        else:
+            return False
